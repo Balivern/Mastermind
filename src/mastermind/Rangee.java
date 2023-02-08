900cc260d191552f283bd424b1e14a9a71a590d8
Lola Condom <lcondom@pcalb-mm0000.univ-jfc.fr>
2023-02-08 12:16:56 +0100

    creation rangee aleatoire

--- src/mastermind/Rangee.java
+++ src/mastermind/Rangee.java
@@ -1,6 +1,7 @@
 package mastermind;
 
 import java.awt.Color;
+import java.util.Random;
 
 public class Rangee {
 	int indicejeton;
@@ -10,10 +11,17 @@ public class Rangee {
 	public Rangee() {
 		this.indicejeton=0;
 		this.jeton = new Color[Modele.DIFFICULTE];
-		
 	}
-	public void evaluation(proposition,combinaison) {
-		
+	public static Rangee aleatoire() {
+		Rangee solution = new Rangee();
+		Random r = new Random();
+		for (solution.indicejeton=0; solution.indicejeton<Modele.DIFFICULTE;solution.indicejeton+=1) {
+			solution.jeton[solution.indicejeton]=Modele.COULEURS[r.nextInt(Modele.COULEURS.length)];
+		}		
+		return solution;
 	}
-
+public static void main(String[] args) {
+	System.out.println(aleatoire());
 }
+}
+
