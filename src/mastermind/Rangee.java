b0a39bd1264e291522540f7416714262c46c21c5
Lola Condom <lcondom@pcalb-mm0000.univ-jfc.fr>
2023-02-08 11:39:49 +0100

    creation rangee

---
+++ src/mastermind/Rangee.java
@@ -0,0 +1,19 @@
+package mastermind;
+
+import java.awt.Color;
+
+public class Rangee {
+	int indicejeton;
+	Color[] jeton;
+	int[] resultat;
+	
+	public Rangee() {
+		this.indicejeton=0;
+		this.jeton = new Color[Modele.DIFFICULTE];
+		
+	}
+	public void evaluation(proposition,combinaison) {
+		
+	}
+
+}
