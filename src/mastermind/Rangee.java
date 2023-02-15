cf9d40ecede812ef93a5c2ffff3bee191cd9d653
Lola Condom <lcondom@pcalb-mm0000.univ-jfc.fr>
2023-02-15 11:17:50 +0100

    debut fct eval

--- src/mastermind/Rangee.java
+++ src/mastermind/Rangee.java
@@ -1,27 +1,45 @@
 package mastermind;
 
 import java.awt.Color;
+import java.util.ArrayList;
 import java.util.Random;
 
 public class Rangee {
 	int indicejeton;
 	Color[] jeton;
 	int[] resultat;
+	Modele m;
 	
-	public Rangee() {
+	public Rangee(Modele mod) {
+		this.m=mod;
 		this.indicejeton=0;
 		this.jeton = new Color[Modele.DIFFICULTE];
 	}
-	public static Rangee aleatoire() {
-		Rangee solution = new Rangee();
+	public  Rangee aleatoire() {
+		Rangee solution = new Rangee(this.m);
 		Random r = new Random();
-		for (solution.indicejeton=0; solution.indicejeton<Modele.DIFFICULTE;solution.indicejeton+=1) {
+		for (solution.indicejeton=0; solution.indicejeton<Modele.DIFFICULTE;solution.indicejeton++) {
 			solution.jeton[solution.indicejeton]=Modele.COULEURS[r.nextInt(Modele.COULEURS.length)];
 		}		
 		return solution;
 	}
-public static void main(String[] args) {
-	System.out.println(aleatoire());
-}
+	public ArrayList evaluation() {
+		ArrayList<Boolean> S = new ArrayList<Boolean>();
+		for (indicejeton=0;indicejeton<Modele.DIFFICULTE;indicejeton++) {
+			if (this.indicejeton==Modele.combinaison.indicejeton) {
+				
+			}
+		}
+	}
+	public String toString() {
+		String retour="";
+		for (int i = 0; i<jeton.length;i++ ) {
+			retour=retour+jeton[i];
+		}
+		return retour;
+		
+	}
+	public static void main(String[] args) {
+	}
 }
 
