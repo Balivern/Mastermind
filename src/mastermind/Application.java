cc042c8aaf6bf1866b1c04183c9dc89bd464c475
Renan Cabane <rcabane@pcalb-mm0000.univ-jfc.fr>
2023-02-15 11:53:33 +0100

    update bandeau

--- src/mastermind/Application.java
+++ src/mastermind/Application.java
@@ -2,24 +2,34 @@ package mastermind;
 
 import java.awt.BorderLayout;
 import java.awt.Canvas;
+import java.awt.Color;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class Application extends JFrame implements WindowListener {
 	
 	Modele modele;
+	JPanel bandeau;
 	Canvas vuePropositions;
 	JPanel vueClavier;
 	JButton [] boutons = new JButton[Modele.COULEURS.length];
 	
 	public Application() {
 		
+		this.bandeau = new JPanel();
+		JLabel titre = new JLabel("  Mastermind, à toi de jouer !");
+		this.bandeau.add(titre);
+		
 		this.vuePropositions = new Canvas();
+		
+		this.vuePropositions.setBackground(Color.WHITE);
+		
 		this.vueClavier = new JPanel();
 		
 		for (int i=0; i<Modele.COULEURS.length; i++) {
@@ -29,13 +39,15 @@ public class Application extends JFrame implements WindowListener {
 			this.boutons[i].setBackground(Modele.COULEURS[i]);
 		}
 		
-		this.setLayout(new BorderLayout());
+		this.setLayout(new BorderLayout(20,10));
 		this.vueClavier.setLayout(new BoxLayout(vueClavier, BoxLayout.X_AXIS));
 		
 		for (int i=0; i<Modele.COULEURS.length; i++) {
 			this.vueClavier.add(this.boutons[i]);
 		}
 		
+		this.add(bandeau, BorderLayout.NORTH);
+		this.add(vuePropositions, BorderLayout.CENTER);
 		this.add(vueClavier, BorderLayout.SOUTH);
 		this.setSize(300,500);
 		this.setVisible(true);
@@ -44,6 +56,12 @@ public class Application extends JFrame implements WindowListener {
 	
 	public static void main(String[] args) {
 		new Application();
+	}
+	
+	public void afficher_proposition() {
+		for (int i=0; i<this.modele.propositions.length; i++) {
+			//this.vuePropositions.add(this.modele.propositions[i]);
+		}
 	}
 	
 	@Override
