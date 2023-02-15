0752ace02d9e248916a1a6135d28784189b4c0d4
Hussameldeen Majid <hmajid@pcalb-mm0000.univ-jfc.fr>
2023-02-15 11:17:59 +0100

    modifie

--- src/mastermind/Modele.java
+++ src/mastermind/Modele.java
@@ -1,9 +1,9 @@
 package mastermind;
 
-import java.awt.Color;
 import java.awt.*;
+import java.util.*;
 
-public class Modele extends Frame {
+public class Modele {
 	
 	static final Color[] COULEURS = {Color.yellow,Color.green,Color.blue,Color.magenta,Color.red,Color.orange,Color.white,Color.black};
 	static final int N_TENTATIVES = 10;
@@ -14,29 +14,31 @@ public class Modele extends Frame {
 		EN_COURS,GAGNE,PERDU
 	}
 	
-	private Etat etat;
-	private Rangee combinaison;
-	private Rangee[] propositions;
-	private int tentative;
+	public Etat etat;
+	public Rangee combinaison;
+	public Rangee[] propositions;
+	public int tentative;
 	
 	
 	public Modele() {
 		this.etat=Etat.EN_COURS;
-		this.combinaison = new Rangee(DIFFICULTE,COULEURS);
-		this.combinaison.TirerRnd();
-		this.propositions = new Rangee[N_TENTATIVES];
+		this.combinaison.aleatoire();
+		this.propositions = new Rangee(this)[N_TENTATIVES];
 		for (int i = 0; i < N_TENTATIVES; i++) {
-			this.propositions[i] = new Rangee(DIFFICULTE, COULEURS);
+			this.propositions[i] = new Rangee();
 			
 		}
 		this.tentative = 0;
 	}
-	
-	public PlatDeJeu() {
-		setTitle("Mastermind");
-		setSize(400,400);
-		setVisible(true);
+	public void initialiserCombinaison() {
+		Random rand = new Random();
+		for (int i = 0; i < DIFFICULTE; i++) {
+			int couleurIndice = rand.nextInt(COULEURS.length);
+			this.combinaison.jeton(i,COULEURS[])
+		}
+		
 	}
+	
 	
 	
 	
